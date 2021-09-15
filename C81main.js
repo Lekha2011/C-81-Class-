f26b422085ad55f66b6d586093c7b45c69d28b54
Lekha2011 <89290762+Lekha2011@users.noreply.github.com>
2021-09-15 18:58:49 +0530

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,40 @@
+canvas= document.getElementById("myCanvas");
+ctx= canvas.getContext("2d");
+
+color="black";
+
+ctx.beginPath();
+ctx.strokeStyle = color; 
+ctx.lineWidth = 2;
+ctx.arc(200,200,140,0,2 * Math.PI);
+ctx.stroke();
+
+canvas.addEventListener("mousedown", my_mousedown);
+
+function my_mousedown (e)
+
+{
+    color=document.getElementById("color").value;
+    console.log(color);
+
+    mousex = e.clientX - canvas.offsetLeft;
+    mousey = e.clientY - canvas.offsetTop;
+
+    console.log("x = " + mousex + " y = " + mousey); 
+    
+    circle(mousex , mousey);
+}
+
+ function circle(mousex , mousey)
+ {
+    ctx.beginPath();
+    ctx.strokeStyle = color; 
+    ctx.lineWidth = 2;
+    ctx.arc(mousex,mousey,140,0,2 * Math.PI);
+    ctx.stroke();
+
+}
+ function clearArea()
+ {
+     ctx.clearRect(0,0,canvas.width,canvas.height);
+ }
